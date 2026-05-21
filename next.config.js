f71bd47374efdea6b7f09a164c5993b304662a40
Tarique Mosharraf <applandsys@gmail.com>
2026-05-22 01:20:06 +0600

    Fix: next config

--- next.config.js
+++ next.config.js
@@ -13,6 +13,18 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'backend.vespop.com',
             },
+            {
+                protocol: 'https',
+                hostname: '94064959ff2f2e1cdff06054b1607835.r2.cloudflarestorage.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'r2.cloudflarestorage.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cloudflarestorage.com',
+            }
         ],
     },
     eslint: {
